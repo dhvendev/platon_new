dfdcd6bb3b8b35f55c1540f1828969db4b116e3d
unknown <kolesnik0vplaton@yandex.ru>
2024-12-30 11:46:15 +0300

    pytests

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -15,3 +15,13 @@ def test_password_characters():
 Тест, что длина пароля соответствует заданной
 Тест, что два сгенерированных подряд пароля различаются
 """
+
+def test_password_len():
+    for length in range(1,100):
+        assert len(generate_password(length)) == length
+
+def test_password_same():
+    length = 12
+    passwords = [generate_password(length) for i in range(1000000)]
+    password = generate_password(length)
+    assert password not in passwords
